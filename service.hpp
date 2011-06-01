befdd37252bbcb6839733b6e32f19f89b158e559
Aleksey Filippov <sarum9in@gmail.com>
2011-06-01 14:30:10 +0400

    New class: bunsan::service.

---
+++ service.hpp
@@ -0,0 +1,30 @@
+#ifndef SERVICE_HPP
+#define SERVICE_HPP
+
+namespace bunsan
+{
+	class service
+	{
+	public:
+		/*!
+		 * \brief start service
+		 * non-blocking call, thread will run in different thread
+		 */
+		virtual void start()=0;
+		/*!
+		 * \brief join service thread
+		 */
+		virtual void join()=0;
+		/*!
+		 * \brief stop service
+		 */
+		virtual void stop()=0;
+		/*!
+		 * \return wether service is running
+		 */
+		virtual bool is_running()=0;
+	};
+}
+
+#endif //SERVICE_HPP
+
