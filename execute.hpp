052c86be7b85e32e21625e140c28abbc7dbd0ce7
Filippov Aleksey <sarum9in@gmail.com>
2011-06-12 02:47:28 +0400

    Added new exec functions: they have support of mapping file to stdin.

--- execute.hpp
+++ execute.hpp
@@ -10,8 +10,19 @@
 
 namespace bunsan
 {
+	int sync_execute(const boost::filesystem::path &cwd, const boost::filesystem::path &file, const std::vector<std::string> &args, const boost::filesystem::path &stdin_file, bool use_path=true);
+	process_ptr async_execute(const boost::filesystem::path &cwd, const boost::filesystem::path &file, const std::vector<std::string> &args, const boost::filesystem::path &stdin_file, bool use_path=true);
 	int sync_execute(const boost::filesystem::path &cwd, const boost::filesystem::path &file, const std::vector<std::string> &args, bool use_path=true);
 	process_ptr async_execute(const boost::filesystem::path &cwd, const boost::filesystem::path &file, const std::vector<std::string> &args, bool use_path=true);
+	// aliases to use with args[0]==file
+	inline int sync_execute(const boost::filesystem::path &cwd, const std::vector<std::string> &args, const boost::filesystem::path &stdin_file, bool use_path=true)
+	{
+		return sync_execute(cwd, args.at(0), args, stdin_file, use_path);
+	}
+	inline process_ptr async_execute(const boost::filesystem::path &cwd, const std::vector<std::string> &args, const boost::filesystem::path &stdin_file, bool use_path=true)
+	{
+		return async_execute(cwd, args.at(0), args, stdin_file, use_path);
+	}
 	inline int sync_execute(const boost::filesystem::path &cwd, const std::vector<std::string> &args, bool use_path=true)
 	{
 		return sync_execute(cwd, args.at(0), args, use_path);
