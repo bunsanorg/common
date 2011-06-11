5395bbe2562c90ca51eb46f020f7056713902446
Aleksey Filippov <sarum9in@gmail.com>
2011-06-11 22:35:49 +0400

    New file: it contains factory functions.

---
+++ factory.hpp
@@ -0,0 +1,39 @@
+#ifndef FACTORY_HPP
+#define FACTORY_HPP
+
+#include <map>
+#include <string>
+#include <functional>
+
+namespace bunsan{namespace factory
+{
+	template <typename Factory>
+	bool register_new(std::map<std::string, Factory> *&factories, const std::string &type, const Factory f)
+	{
+		if (!factories)
+			factories = new std::map<std::string, Factory>;
+		if (factories->find(type)==factories->end())
+		{
+			(*factories)[type] = f;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	template <typename Factory, typename ... Args>
+	auto instance(std::map<std::string, Factory> *&factories, const std::string &type, Args ... args)->decltype(factories->find(type)->second(args...))
+	{
+		if (factories)
+		{
+			auto iter = factories->find(type);
+			if (iter!=factories->end())
+				return iter->second(args...);
+		}
+		return decltype(factories->find(type)->second(args...))();
+	}
+}}
+
+#endif //FACTORY_HPP
+
