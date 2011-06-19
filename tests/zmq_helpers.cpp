0deb0c8e9d3066b2ed873c5d90782509f51e09f9
Aleksey Filippov <sarum9in@gmail.com>
2011-06-19 20:03:39 +0400

    Unit tests was added. Bug in enum_cast was found. Function was removed as useless (bug can't be solved).

---
+++ tests/zmq_helpers.cpp
@@ -0,0 +1,23 @@
+#include "zmq_helpers.hpp"
+
+int main()
+{
+	using namespace bunsan::zmq_helpers;
+	zmq::message_t message;
+	std::string s;
+	std::vector<std::string> ss;
+	std::string msg = "some strange text\nwith\nstrange\t\t\tsymbols\n\n";
+	encode(msg, message);
+	decode(message, s);
+	assert(msg==s);
+	std::vector<std::string> msgs0 = {"msg1\n\n", "msg2", "dcjnjvcs\n\n\ndmklc", "o_O", "\n\n\nakmdad", "", "the end!!!111"};
+	encode(msgs0, message);
+	decode(message, ss);
+	assert(msgs0==ss);
+	std::vector<std::string> msgs1 = {"", "", "o_O", ""};
+	encode(msgs1, message);
+	decode(message, ss);
+	assert(msgs1==ss);
+	return 0;
+}
+
