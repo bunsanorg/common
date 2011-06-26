270d934d7f2e17623aa2b29d80d7022675844ba1
Aleksey Filippov <sarum9in@gmail.com>
2011-06-26 21:37:42 +0400

    Old code deletion (previously was commented). use_path variable support in bunsan::executor config. New unit test.

---
+++ tests/executor_path.cpp
@@ -0,0 +1,47 @@
+#include <sstream>
+
+#include <cassert>
+
+#include <boost/property_tree/info_parser.hpp>
+
+#include "bunsan/executor.hpp"
+
+int main()
+{
+	using namespace boost::property_tree;
+	using bunsan::executor;
+	using bunsan::process::context;
+	typedef std::stringstream ss;
+	typedef std::vector<std::string> vs;
+	ptree pt;
+	context ctx;
+	ss in_1("d\n"
+		"{\n"
+		"\tuse_path 1\n"
+		"\texecutable exe\n"
+		"\tcurrent_path path\n"
+		"}\n");
+	read_info(in_1, pt);
+	ctx = executor(pt).context();
+	assert(ctx.executable()=="exe");
+	assert(ctx.current_path()=="path");
+	assert(ctx.use_path());
+	ss in_2("d\n"
+		"{\n"
+		"\texecutable exe\n"
+		"\tuse_path 0\n"
+		"}\n");
+	read_info(in_2, pt);
+	ctx = executor(pt).context();
+	assert(ctx.executable()=="exe");
+	assert(!ctx.use_path());
+	ss in_3("d\n"
+		"{\n"
+		"\texecutable /bin/exe\n"
+		"}\n");
+	read_info(in_3, pt);
+	ctx = executor(pt).context();
+	assert(ctx.executable()=="/bin/exe");
+	assert(!ctx.use_path());
+}
+
