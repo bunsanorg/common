96432353cee1029a990af58b3bbad9f5c32e3564
Aleksey Filippov <sarum9in@gmail.com>
2011-06-26 22:05:05 +0400

    bunsan::process::context fixes, build reimplementation. executor unit tests fixes.

--- tests/executor_env.cpp
+++ tests/executor_env.cpp
@@ -15,13 +15,14 @@ int main()
 	typedef std::vector<std::string> vs;
 	ptree pt;
 	context ctx;
-	ss in_1("d\n"
+	ss in_1("t 0\n"
+		"d\n"
 		"{\n"
 		"\texecutable exe\n"
 		"\tcurrent_path path\n"
 		"}\n");
 	read_info(in_1, pt);
-	ctx = executor(pt).context();
+	ctx = executor(pt).context().built();
 	assert(ctx.executable()=="exe");
 	assert(ctx.current_path()=="path");
 }
--- tests/executor_path.cpp
+++ tests/executor_path.cpp
@@ -15,32 +15,35 @@ int main()
 	typedef std::vector<std::string> vs;
 	ptree pt;
 	context ctx;
-	ss in_1("d\n"
+	ss in_1("t 0\n"
+		"d\n"
 		"{\n"
 		"\tuse_path 1\n"
 		"\texecutable exe\n"
 		"\tcurrent_path path\n"
 		"}\n");
 	read_info(in_1, pt);
-	ctx = executor(pt).context();
+	ctx = executor(pt).context().built();
 	assert(ctx.executable()=="exe");
 	assert(ctx.current_path()=="path");
 	assert(ctx.use_path());
-	ss in_2("d\n"
+	ss in_2("t 0\n"
+		"d\n"
 		"{\n"
 		"\texecutable exe\n"
 		"\tuse_path 0\n"
 		"}\n");
 	read_info(in_2, pt);
-	ctx = executor(pt).context();
-	assert(ctx.executable()=="exe");
+	ctx = executor(pt).context().built();
+	assert(ctx.executable().filename()=="exe" && ctx.executable().is_absolute());
 	assert(!ctx.use_path());
-	ss in_3("d\n"
+	ss in_3("t 0\n"
+		"d\n"
 		"{\n"
 		"\texecutable /bin/exe\n"
 		"}\n");
 	read_info(in_3, pt);
-	ctx = executor(pt).context();
+	ctx = executor(pt).context().built();
 	assert(ctx.executable()=="/bin/exe");
 	assert(!ctx.use_path());
 }
