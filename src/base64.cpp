0193d2827af319d8cae29ad592ead1670fa8ee32
Filippov Aleksey <sarum9in@gmail.com>
2011-10-05 13:22:17 +0400

    New bunsan::base64::encode function with std::string argument and unit tests for this function.

--- src/base64.cpp
+++ src/base64.cpp
@@ -7,6 +7,12 @@ std::string bunsan::base64::encode(const std::vector<unsigned char> &data)
 	return xmlrpc_c::base64FromBytes(data);
 }
 
+std::string bunsan::base64::encode(const std::string &data)
+{
+	std::vector<unsigned char> data_(data.begin(), data.end());
+	return encode(data_);
+}
+
 std::vector<unsigned char> bunsan::base64::decode(const std::string &base64)
 {
 	return xmlrpc_c::bytesFromBase64(base64);
