fc7640491c814088363b4dfa0be153ff17132855
Aleksey Filippov <sarum9in@gmail.com>
2011-10-15 23:51:59 +0400

    New function: bunsan::system_error.

---
+++ include/bunsan/system_error.hpp
@@ -0,0 +1,16 @@
+#ifndef BUNSAN_SYSTEM_ERROR_HPP
+#define BUNSAN_SYSTEM_ERROR_HPP
+
+#include <system_error>
+#include <string>
+
+namespace bunsan
+{
+	inline void system_error(const std::string &msg)
+	{
+		throw std::system_error(std::error_code(errno, std::system_category()), msg);
+	}
+}
+
+#endif //BUNSAN_SYSTEM_ERROR_HPP
+
