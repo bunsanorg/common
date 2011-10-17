e9e408ffe00522e769625fcfa7187d89e4099475
Aleksey Filippov <sarum9in@gmail.com>
2011-10-17 20:53:05 +0400

    Bug fix in ptree_get unit test.

--- tests/ptree_get.cpp
+++ tests/ptree_get.cpp
@@ -11,7 +11,8 @@ int main()
 	pt.get_child("some.path")==vl;
 	assert(bunsan::ptree_get(pt, "some.path", vl2)==vl);
 	bunsan::ptree_get(pt, "some.path").put("name0", "value0");
-	bunsan::ptree_get(pt, "some.path").put("name0", "value1");
+	bunsan::ptree_get(pt, "some.path").put("name1", "value1");
+	boost::property_tree::write_info(std::cout, pt);
 	assert(pt.get<std::string>("some.path.name0")=="value0");
 	assert(pt.get<std::string>("some.path.name1")=="value1");
 }
