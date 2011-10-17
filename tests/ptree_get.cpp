3cc784f88dd6bc304a05fe779f44d7f644b451b6
Aleksey Filippov <sarum9in@gmail.com>
2011-10-17 20:46:58 +0400

    New unit tests for bunsan::ptree_get (failed).

--- tests/ptree_get.cpp
+++ tests/ptree_get.cpp
@@ -4,9 +4,15 @@
 
 int main()
 {
-	boost::property_tree::ptree pt, vl;
+	boost::property_tree::ptree pt, vl, vl2;
 	vl.put_value("231dasmnw");
-	bunsan::ptree_get(pt, "some.path", vl)==vl;
+	vl2.put_value("njskdnjsdnjkasd");
+	assert(bunsan::ptree_get(pt, "some.path", vl)==vl);
 	pt.get_child("some.path")==vl;
+	assert(bunsan::ptree_get(pt, "some.path", vl2)==vl);
+	bunsan::ptree_get(pt, "some.path").put("name0", "value0");
+	bunsan::ptree_get(pt, "some.path").put("name0", "value1");
+	assert(pt.get<std::string>("some.path.name0")=="value0");
+	assert(pt.get<std::string>("some.path.name1")=="value1");
 }
 
