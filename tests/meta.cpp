5e13dffcf03c6f1d80ac46caff7f29b898a2a954
Aleksey Filippov <sarum9in@gmail.com>
2011-11-24 17:12:33 +0400

    New sublibrary: bunsan::meta.

---
+++ tests/meta.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "bunsan/meta/comparable.hpp"
+
+namespace
+{
+	struct stub
+	{
+	};
+}
+
+int main()
+{
+	using namespace bunsan;
+	static_assert(meta::is_less_than_comparable<int>::value, "is_less_than_comparable");
+	static_assert(!meta::is_equality_comparable<stub>::value, "is_equality_comparable");
+	static_assert(meta::is_greater_than_comparable<std::string>::value, "is_greater_than_comparable");
+}
+
