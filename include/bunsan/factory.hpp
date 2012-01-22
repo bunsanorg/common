082890e285ecc0399eb530eea258c41bf342840b
Filippov Aleksey <sarum9in@gmail.com>
2012-01-23 00:46:09 +0400

    bunsan::factory refactoring.
    Minor bug fixes + readability improvements.

--- include/bunsan/factory.hpp
+++ include/bunsan/factory.hpp
@@ -4,27 +4,32 @@
 #include <map>
 #include <string>
 #include <functional>
+#include <type_traits>
 
 namespace bunsan{namespace factory
 {
 	template <typename Factory>
-	bool register_new(std::map<std::string, Factory> *&factories, const std::string &type, const Factory f)
+	bool register_new(
+		std::map<std::string, Factory> *&factories,
+		const std::string &type,
+		const Factory &f)
 	{
+		typedef std::map<std::string, Factory> map_type;
+		typedef typename map_type::value_type value_type;
 		if (!factories)
-			factories = new std::map<std::string, Factory>;
+			factories = new map_type;
 		if (factories->find(type)==factories->end())
 		{
-			(*factories)[type] = f;
+			factories->insert(value_type(type, f));
 			return true;
 		}
-		else
-		{
-			return false;
-		}
+		return false;
 	}
 	template <typename Factory, typename ... Args>
-	auto instance(std::map<std::string, Factory> *&factories, const std::string &type, Args &&...args)
-		->decltype(factories->find(type)->second(std::forward<Args>(args)...))
+	typename std::result_of<Factory(Args &&...)>::type instance(
+		std::map<std::string, Factory> *&factories,
+		const std::string &type,
+		Args &&...args)
 	{
 		if (factories)
 		{
@@ -32,7 +37,7 @@ namespace bunsan{namespace factory
 			if (iter!=factories->end())
 				return iter->second(std::forward<Args>(args)...);
 		}
-		return decltype(factories->find(type)->second(std::forward<Args>(args)...))();
+		return typename std::result_of<Factory(Args &&...)>::type();
 	}
 }}
 
