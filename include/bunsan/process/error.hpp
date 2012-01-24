42ce2494e1e219e0ce0bdf8f47e83cd8731bf61f
Filippov Aleksey <sarum9in@gmail.com>
2012-01-25 02:38:22 +0400

    bunsan::process refactoring, exception classes implementation.
    bunsan::process::check_sync_execute implementation.
    bunsan::process::error exception implementation.
    bunsan::process::non_zero_exit_status_error exception implementation.

---
+++ include/bunsan/process/error.hpp
@@ -0,0 +1,23 @@
+#ifndef BUNSAN_PROCESS_ERROR_HPP
+#define BUNSAN_PROCESS_ERROR_HPP
+
+#include "bunsan/error.hpp"
+
+namespace bunsan{namespace process
+{
+	struct error: virtual bunsan::error
+	{
+		explicit error(const std::string &msg);
+	};
+	class non_zero_exit_status_error: public virtual error
+	{
+	public:
+		explicit non_zero_exit_status_error(int exit_status_);
+		virtual int exit_status() const throw();
+	private:
+		typedef boost::error_info<struct error_exit_status_tag, int> error_exit_status;
+	};
+}}
+
+#endif //BUNSAN_PROCESS_ERROR_HPP
+
