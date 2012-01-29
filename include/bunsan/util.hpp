b932f5b4882ca41904afb8bfc54874eb08e72c41
Filippov Aleksey <sarum9in@gmail.com>
2012-01-30 02:55:39 +0400

    bunsan/util.hpp include fix.

--- include/bunsan/util.hpp
+++ include/bunsan/util.hpp
@@ -20,7 +20,7 @@
 
 #include <boost/optional.hpp>
 #include <boost/filesystem/path.hpp>
-#include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
 
