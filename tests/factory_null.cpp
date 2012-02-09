cddc8246f1efecc622902f6ee363ad15c8348816
Filippov Aleksey <sarum9in@gmail.com>
2012-02-09 19:49:05 +0400

    factory_null unit test implementation.

---
+++ tests/factory_null.cpp
@@ -0,0 +1,17 @@
+#include "bunsan/factory.hpp"
+
+#include <memory>
+#include <set>
+
+#include <boost/assert.hpp>
+
+int main()
+{
+	typedef bunsan::factory<std::function<std::shared_ptr<int>()>> bunsan_factory;
+	typename bunsan_factory::map_type *map(0);
+	std::set<bunsan_factory::key_type> set(
+		bunsan_factory::registered_begin(map),
+		bunsan_factory::registered_end(map));
+	BOOST_ASSERT(set.empty());
+}
+
