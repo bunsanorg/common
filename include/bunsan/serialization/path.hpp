788758024fd66c989fa882464bf646f49bf21b2d
Aleksey Filippov <sarum9in@gmail.com>
2012-11-12 15:37:19 +0400

    path serialization bug fix.

--- include/bunsan/serialization/path.hpp
+++ include/bunsan/serialization/path.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <boost/serialization/split_free.hpp>
+#include <boost/serialization/nvp.hpp>
 
 #include <boost/filesystem/path.hpp>
 
@@ -8,18 +9,17 @@ BOOST_SERIALIZATION_SPLIT_FREE(boost::filesystem::path)
 
 namespace boost{namespace serialization
 {
-    // FIXME nvp
     template <typename Archive>
     void save(Archive &ar, const boost::filesystem::path &path, const unsigned int /*version*/)
     {
-        ar & path.generic_string();
+        ar & boost::serialization::make_nvp("path", path.generic_string());
     }
 
     template <typename Archive>
     void load(Archive &ar, boost::filesystem::path &path, const unsigned int /*version*/)
     {
         std::string str;
-        ar & str;
+        ar & boost::serialization::make_nvp("path", str);
         path = str;
     }
 }}
