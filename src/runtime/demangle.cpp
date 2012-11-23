8c9b1bd1f9846af18b21f5ef0fd1880ab4d56a6e
Filippov Aleksey <sarum9in@gmail.com>
2012-11-23 15:34:36 +0400

    runtime::demangle() implementation.

---
+++ src/runtime/demangle.cpp
@@ -0,0 +1,48 @@
+#include "bunsan/runtime/demangle.hpp"
+
+#if defined(__GLIBCXX__) || defined(__GLIBCPP__)
+#include <boost/scope_exit.hpp>
+
+#include <cxxabi.h>
+
+namespace bunsan{namespace runtime
+{
+    std::string demangle(const char *const name)
+    {
+        char *realname = nullptr;
+        BOOST_SCOPE_EXIT_ALL(realname)
+        {
+            if (realname)
+                std::free(realname);
+        };
+
+        std::size_t len;
+        int stat;
+
+        realname = abi::__cxa_demangle(name, nullptr, &len, &stat);
+
+        if (realname)
+            return std::string(realname);
+        else
+            return name;
+    }
+
+    std::string demangle(const std::string &name)
+    {
+        return demangle(name.c_str());
+    }
+}}
+#else
+namespace bunsan{namespace runtime
+{
+    std::string demangle(const char *const name)
+    {
+        return name;
+    }
+
+    std::string demangle(const std::string &name)
+    {
+        return name;
+    }
+}}
+#endif
