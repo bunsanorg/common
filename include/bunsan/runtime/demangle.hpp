c6a1f7922aea20c3ca58bf8d7858c8735795e3c5
Aleksey Filippov <sarum9in@gmail.com>
2012-11-23 15:34:36 +0400

    runtime::demangle() implementation.

---
+++ include/bunsan/runtime/demangle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <typeinfo>
+
+namespace bunsan{namespace runtime
+{
+    std::string demangle(const char *const name);
+    std::string demangle(const std::string &name);
+
+    template <typename T>
+    std::string name(const T &obj)
+    {
+        return demangle(typeid(obj).name());
+    }
+}}
