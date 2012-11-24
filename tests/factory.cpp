c6865a0be436ffbd3752a0635a1f554d451ed7f1
Aleksey Filippov <sarum9in@gmail.com>
2012-11-24 16:21:41 +0400

    factory test suite refactoring.

--- tests/factory.cpp
+++ tests/factory.cpp
@@ -1,28 +1,42 @@
-#include <memory>
-#include <set>
-
-#include <cassert>
+#define BOOST_TEST_MODULE factory
+#include <boost/test/unit_test.hpp>
 
 #include "bunsan/factory.hpp"
 
-int main()
+#include <memory>
+#include <set>
+
+BOOST_AUTO_TEST_SUITE(factory)
+
+BOOST_AUTO_TEST_CASE(basic)
 {
     typedef std::shared_ptr<int> int_ptr;
     typedef bunsan::factory<int_ptr()> bunsan_factory;
     typename bunsan_factory::map_type *map(0);
-    assert(!static_cast<bool>(bunsan_factory::instance(map, "strange")));
-    assert(  bunsan_factory::register_new(map, "zero", [](){return int_ptr(new int(0));} )  );
-    assert(map!=0);
-    assert(!  bunsan_factory::register_new(map, "zero", [](){return int_ptr(new int(-1));} )  );
-    assert(*bunsan_factory::instance(map, "zero")==0);
-    assert(  bunsan_factory::register_new(map, "one", [](){return int_ptr(new int(1));} )  );
-    assert(*bunsan_factory::instance(map, "zero")==0);
-    assert(*bunsan_factory::instance(map, "one")==1);
-    assert(!static_cast<bool>(bunsan_factory::instance(map, "")));
+    BOOST_CHECK(!static_cast<bool>(bunsan_factory::instance(map, "strange")));
+    BOOST_CHECK(  bunsan_factory::register_new(map, "zero", [](){return int_ptr(new int(0));} )  );
+    BOOST_CHECK_NE(map, static_cast<typename bunsan_factory::map_type *>(nullptr));
+    BOOST_CHECK(!  bunsan_factory::register_new(map, "zero", [](){return int_ptr(new int(-1));} )  );
+    BOOST_CHECK_EQUAL(*bunsan_factory::instance(map, "zero"), 0);
+    BOOST_CHECK(  bunsan_factory::register_new(map, "one", [](){return int_ptr(new int(1));} )  );
+    BOOST_CHECK_EQUAL(*bunsan_factory::instance(map, "zero"), 0);
+    BOOST_CHECK_EQUAL(*bunsan_factory::instance(map, "one"), 1);
+    BOOST_CHECK(!static_cast<bool>(bunsan_factory::instance(map, "")));
     {
         std::set<std::string> set(bunsan_factory::registered_begin(map), bunsan_factory::registered_end(map));
         std::set<std::string> req = {"zero", "one"};
-        assert(req==set);
+        BOOST_CHECK(req == set);
     }
-    return 0;
 }
+
+BOOST_AUTO_TEST_CASE(null)
+{
+    typedef bunsan::factory<std::shared_ptr<int>()> bunsan_factory;
+    typename bunsan_factory::map_type *map(0);
+    std::set<bunsan_factory::key_type> set(
+        bunsan_factory::registered_begin(map),
+        bunsan_factory::registered_end(map));
+    BOOST_CHECK(set.empty());
+}
+
+BOOST_AUTO_TEST_SUITE_END() // factory
