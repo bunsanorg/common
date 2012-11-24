975ea8c3a9aed7a03538dfb0df12d9ab697b467c
Filippov Aleksey <sarum9in@gmail.com>
2012-11-24 16:24:01 +0400

    meta test suite refactoring.

--- tests/meta.cpp
+++ tests/meta.cpp
@@ -1,18 +1,21 @@
+#define BOOST_TEST_MODULE meta
+#include <boost/test/unit_test.hpp>
+
 #include <string>
 
 #include "bunsan/meta/comparable.hpp"
 
-namespace
+BOOST_AUTO_TEST_SUITE(meta)
+
+struct stub
 {
-    struct stub
-    {
-    };
+};
+
+BOOST_AUTO_TEST_CASE(comparable)
+{
+    static_assert(bunsan::meta::is_less_than_comparable<int>::value, "is_less_than_comparable");
+    static_assert(!bunsan::meta::is_equality_comparable<stub>::value, "is_equality_comparable");
+    static_assert(bunsan::meta::is_greater_than_comparable<std::string>::value, "is_greater_than_comparable");
 }
 
-int main()
-{
-    using namespace bunsan;
-    static_assert(meta::is_less_than_comparable<int>::value, "is_less_than_comparable");
-    static_assert(!meta::is_equality_comparable<stub>::value, "is_equality_comparable");
-    static_assert(meta::is_greater_than_comparable<std::string>::value, "is_greater_than_comparable");
-}
+BOOST_AUTO_TEST_SUITE_END() // meta
