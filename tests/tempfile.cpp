17e4f17fb8dfdc93bc2c783243792626b7b644d6
Filippov Aleksey <sarum9in@gmail.com>
2012-11-24 14:53:29 +0400

    tempfile test suite refactoring.

--- tests/tempfile.cpp
+++ tests/tempfile.cpp
@@ -1,8 +1,13 @@
+#define BOOST_TEST_MODULE tempfile
+#include <boost/test/unit_test.hpp>
+
 #include "bunsan/tempfile.hpp"
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <boost/assert.hpp>
+
+BOOST_AUTO_TEST_SUITE(tempfile)
 
 using bunsan::tempfile;
 using boost::filesystem::path;
@@ -27,36 +32,44 @@ template <typename T>
 void touch(const T &file)
 {
     boost::filesystem::ofstream tmp(getpath(file));
+    tmp.close();
+    BOOST_REQUIRE(tmp);
+    BOOST_REQUIRE(exists(file));
 }
 
-int main()
+BOOST_AUTO_TEST_CASE(auto_remove)
 {
     boost::filesystem::path tmppath;
     {
         tempfile tmp = tempfile::unique();
         tmppath = tmp.path();
-        BOOST_ASSERT(tmp.auto_remove());
-        BOOST_ASSERT(!exists(tmp));
+        BOOST_CHECK(tmp.auto_remove());
+        BOOST_CHECK(!exists(tmp));
         touch(tmp);
-        BOOST_ASSERT(exists(tmp));
     }
-    BOOST_ASSERT(!exists(tmppath));
-    {
-        tempfile tmp = tempfile::unique();
-        tmppath = tmp.path();
-        touch(tmp);
-        BOOST_ASSERT(exists(tmp));
-        tempfile tmp2;
-        BOOST_ASSERT(tmp.auto_remove());
-        BOOST_ASSERT(!tmp2.auto_remove());
-        swap(tmp, tmp2);
-        BOOST_ASSERT(!tmp.auto_remove());
-        BOOST_ASSERT(tmp2.auto_remove());
-        BOOST_ASSERT(tmp2.path()==tmppath);
-    }
-    {
-        tempfile tmp = tempfile::unique("%%%%-%%%%-%%%%-%%%%.tgz");
-        touch(tmp);
-        BOOST_ASSERT(tmp.path().extension()==".tgz");
-    }
+    BOOST_CHECK(!exists(tmppath));
 }
+
+BOOST_AUTO_TEST_CASE(swap_)
+{
+    boost::filesystem::path tmppath;
+    tempfile tmp = tempfile::unique();
+    tmppath = tmp.path();
+    touch(tmp);
+    tempfile tmp2;
+    BOOST_CHECK(tmp.auto_remove());
+    BOOST_CHECK(!tmp2.auto_remove());
+    swap(tmp, tmp2);
+    BOOST_CHECK(!tmp.auto_remove());
+    BOOST_CHECK(tmp2.auto_remove());
+    BOOST_CHECK(tmp2.path()==tmppath);
+}
+
+BOOST_AUTO_TEST_CASE(model)
+{
+    tempfile tmp = tempfile::unique("%%%%-%%%%-%%%%-%%%%.tgz");
+    touch(tmp);
+    BOOST_CHECK_EQUAL(tmp.path().extension(), ".tgz");
+}
+
+BOOST_AUTO_TEST_SUITE_END() // tempfile
