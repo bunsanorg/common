42cbfd579fb9fe2d763b3985eaaf0bdcc6b2553e
Filippov Aleksey <sarum9in@gmail.com>
2012-11-24 19:56:55 +0400

    get(): excess overloads were removed.

--- tests/get.cpp
+++ tests/get.cpp
@@ -1,92 +1,22 @@
+#define BOOST_TEST_MODULE get
+#include <boost/test/unit_test.hpp>
+
 #include "bunsan/get.hpp"
-#include "bunsan/util.hpp"
 
-#include <string>
-
-#include <cassert>
-#include <cstring>
-
-#define test_m \
-    try \
+#define BUNSAN_CHECK_EXCEPTION(OPT, MSG) BOOST_CHECK_EXCEPTION(bunsan::get(OPT, MSG), bunsan::uninitialized_optional_error, \
+    [](const bunsan::uninitialized_optional_error &e) \
     { \
-        bunsan::get(o, msg); \
-        assert(o); \
-        SLOG("optional is initialized"); \
-    } \
-    catch (std::invalid_argument &e) \
-    { \
-        assert(!o); \
-        assert(msg==e.what()); \
-        SLOG("caught: "<<e.what()); \
-    }
+        return *e.get<bunsan::error::message>() == MSG; \
+    })
 
-#define test_e_ \
-    try \
-    { \
-        bunsan::get(o, msg); \
-        assert(o); \
-        SLOG("optional is initialized"); \
-    } \
-    catch (E &e) \
+BOOST_AUTO_TEST_SUITE(get)
 
-#define test_ee test_e_ \
-    { \
-        assert(!o); \
-        assert(e==msg); \
-        SLOG("caught: "<<e); \
-    }
-
-#define test_se test_e_ \
-    { \
-        assert(!o); \
-        assert(!strcmp(e.what(), msg.what())); \
-        SLOG("caught: "<<e.what()); \
-    }
-
-template <typename T>
-void test_msg(boost::optional<T> &o, const std::string &msg)
-{
-    test_m
-}
-
-template <typename T>
-void test_msg(const boost::optional<T> &o, const std::string &msg)
-{
-    test_m
-}
-
-template <typename T, typename E>
-void test_sexcept(boost::optional<T> &o, const E &msg)
-{
-    test_se
-}
-
-template <typename T, typename E>
-void test_sexcept(const boost::optional<T> &o, const E &msg)
-{
-    test_se
-}
-
-template <typename T, typename E>
-void test_except(boost::optional<T> &o, const E &msg)
-{
-    test_ee
-}
-
-template <typename T, typename E>
-void test_except(const boost::optional<T> &o, const E &msg)
-{
-    test_ee
-}
-
-int main()
+BOOST_AUTO_TEST_CASE(optional)
 {
     boost::optional<int> o;
-    test_msg(o, "adsdkmas wkdmnakjndjkasnjd");
-    test_except(o, 100500);
-    test_sexcept(o, std::logic_error("jdknasjkdnc amk mkamnaklm"));
+    BUNSAN_CHECK_EXCEPTION(o, "adsdkmas wkdmnakjndjkasnjd");
     o = 10;
-    test_msg(o, "dnjasndkasndjknasjkdn");
-    test_except(o, 100500);
-    test_sexcept(o, std::logic_error("jdknasjkdnc amk mkamnaklm"));
+    BOOST_CHECK_EQUAL(bunsan::get(o, "dnjasndkasndjknasjkdn"), 10);
 }
+
+BOOST_AUTO_TEST_SUITE_END() // get
