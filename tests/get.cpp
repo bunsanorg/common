064fae0043a561919f119a0ddd93ab848cb4e0af
Filippov Aleksey <sarum9in@gmail.com>
2012-11-24 19:58:53 +0400

    get and ptree_get test suites were merged.

--- tests/get.cpp
+++ tests/get.cpp
@@ -19,4 +19,18 @@ BOOST_AUTO_TEST_CASE(optional)
     BOOST_CHECK_EQUAL(bunsan::get(o, "dnjasndkasndjknasjkdn"), 10);
 }
 
+BOOST_AUTO_TEST_CASE(ptree)
+{
+    boost::property_tree::ptree pt, vl, vl2;
+    vl.put_value("231dasmnw");
+    vl2.put_value("njskdnjsdnjkasd");
+    BOOST_CHECK(bunsan::ptree_get(pt, "some.path", vl) == vl);
+    pt.get_child("some.path")==vl;
+    BOOST_CHECK(bunsan::ptree_get(pt, "some.path", vl2) == vl);
+    bunsan::ptree_get(pt, "some.path").put("name0", "value0");
+    bunsan::ptree_get(pt, "some.path").put("name1", "value1");
+    BOOST_CHECK_EQUAL(pt.get<std::string>("some.path.name0"), "value0");
+    BOOST_CHECK_EQUAL(pt.get<std::string>("some.path.name1"), "value1");
+}
+
 BOOST_AUTO_TEST_SUITE_END() // get
