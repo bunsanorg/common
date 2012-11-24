53a288a24f604233989630d499aa075bac3a902e
Aleksey Filippov <sarum9in@gmail.com>
2012-11-24 16:22:08 +0400

    ptree_get test suite refactoring.

--- tests/ptree_get.cpp
+++ tests/ptree_get.cpp
@@ -1,18 +1,23 @@
+#define BOOST_TEST_MODULE ptree_get
+#include <boost/test/unit_test.hpp>
+
 #include "bunsan/util.hpp"
 
-#include <cassert>
+BOOST_AUTO_TEST_SUITE(ptree_get)
 
-int main()
+BOOST_AUTO_TEST_CASE(ptree_get)
 {
     boost::property_tree::ptree pt, vl, vl2;
     vl.put_value("231dasmnw");
     vl2.put_value("njskdnjsdnjkasd");
-    assert(bunsan::ptree_get(pt, "some.path", vl)==vl);
+    BOOST_CHECK(bunsan::ptree_get(pt, "some.path", vl) == vl);
     pt.get_child("some.path")==vl;
-    assert(bunsan::ptree_get(pt, "some.path", vl2)==vl);
+    BOOST_CHECK(bunsan::ptree_get(pt, "some.path", vl2) == vl);
     bunsan::ptree_get(pt, "some.path").put("name0", "value0");
     bunsan::ptree_get(pt, "some.path").put("name1", "value1");
     boost::property_tree::write_info(std::cout, pt);
-    assert(pt.get<std::string>("some.path.name0")=="value0");
-    assert(pt.get<std::string>("some.path.name1")=="value1");
+    BOOST_CHECK_EQUAL(pt.get<std::string>("some.path.name0"), "value0");
+    BOOST_CHECK_EQUAL(pt.get<std::string>("some.path.name1"), "value1");
 }
+
+BOOST_AUTO_TEST_SUITE_END() // ptree_get
