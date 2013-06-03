a041c38e6b18335fbab38a7cedafbb1c9e2a3ee8
Aleksey Filippov <sarum9in@gmail.com>
2013-06-04 00:40:18 +0400

    bunsan_protobuf_generate_cpp() refactoring.

--- BunsanCommon.cmake
+++ BunsanCommon.cmake
@@ -132,12 +132,13 @@ endmacro()
 
 # TODO refactoring needed
 function(bunsan_protobuf_generate_cpp)
-    include_directories(${CMAKE_CURRENT_BINARY_DIR})
     set(options)
     set(one_value_args HEADERS SOURCES INSTALL)
     set(multi_value_args PROTOS)
     cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-    set(protoc_cmd ${PROTOBUF_PROTOC_EXECUTABLE} --cpp_out=${CMAKE_CURRENT_BINARY_DIR} --proto_path=${CMAKE_CURRENT_SOURCE_DIR}/include)
+    set(proto_src ${CMAKE_CURRENT_SOURCE_DIR}/include)
+    set(proto_dst ${CMAKE_CURRENT_BINARY_DIR}/bunsan_protobuf)
+    include_directories(${proto_dst})
     set(srcs_)
     set(hdrs_)
     set(protos_)
@@ -145,9 +146,9 @@ function(bunsan_protobuf_generate_cpp)
         get_filename_component(filename_we ${proto} NAME_WE)
         get_filename_component(dirname ${proto} PATH)
         set(path_we ${dirname}/${filename_we})
-        set(hdr ${CMAKE_CURRENT_BINARY_DIR}/${path_we}.pb.h)
-        set(src ${CMAKE_CURRENT_BINARY_DIR}/${path_we}.pb.cc)
-        set(prt ${CMAKE_CURRENT_SOURCE_DIR}/include/${proto})
+        set(hdr ${proto_dst}/${path_we}.pb.h)
+        set(src ${proto_dst}/${path_we}.pb.cc)
+        set(prt ${proto_src}/${proto})
         list(APPEND hdrs_ ${hdr})
         list(APPEND srcs_ ${src})
         list(APPEND protos_ ${prt})
@@ -162,9 +163,13 @@ function(bunsan_protobuf_generate_cpp)
         list(APPEND proto_paths --proto_path=${proto_path})
     endforeach()
     add_custom_command(
+        OUTPUT ${proto_dst}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${proto_dst}
+    )
+    add_custom_command(
         OUTPUT ${hdrs_} ${srcs_}
-        COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} --cpp_out=${CMAKE_CURRENT_BINARY_DIR} ${proto_paths} ${protos_}
-        DEPENDS ${protos_}
+        COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} --cpp_out=${proto_dst} ${proto_paths} ${protos_}
+        DEPENDS ${protos_} ${proto_dst}
     )
     set(${ARG_SOURCES} ${srcs_} PARENT_SCOPE)
     set(${ARG_HEADERS} ${hdrs_} PARENT_SCOPE)
