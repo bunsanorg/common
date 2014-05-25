5c6ddabdbfcd910525f468c9eb62cb024b555349
Aleksey Filippov <sarum9in@gmail.com>
2014-05-26 03:27:00 +0400

    property_tree::read_info() style fix.

--- include/bunsan/property_tree/info_parser.hpp
+++ include/bunsan/property_tree/info_parser.hpp
@@ -13,14 +13,27 @@ namespace bunsan{namespace property_tree
     /*!
      * \brief Read info from filename with relative path bug fix
      *
-     * \warning This function is not safe, it should be used only from single thread.
+     * \warning This function is not safe,
+     * it should be used only from single thread.
      */
     template <typename Ptree>
-    void read_info(const std::string &filename, Ptree &pt, const std::locale &loc = std::locale())
+    void read_info(const std::string &filename,
+                   Ptree &pt,
+                   const std::locale &loc = std::locale())
     {
         boost::filesystem::initial_path();
-        boost::filesystem::current_path(boost::filesystem::absolute(boost::filesystem::path(filename)).parent_path());
-        boost::property_tree::info_parser::read_info(boost::filesystem::path(filename).filename().string(), pt, loc);
-        boost::filesystem::current_path(boost::filesystem::initial_path());
+        boost::filesystem::current_path(
+            boost::filesystem::absolute(
+                boost::filesystem::path(filename)
+            ).parent_path()
+        );
+        boost::property_tree::info_parser::read_info(
+            boost::filesystem::path(filename).filename().string(),
+            pt,
+            loc
+        );
+        boost::filesystem::current_path(
+            boost::filesystem::initial_path()
+        );
     }
 }}
