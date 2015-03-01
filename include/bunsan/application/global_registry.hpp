24837d37d7d35d0c7364c41f8fe95233ff906023
Aleksey Filippov <sarum9in@gmail.com>
2015-03-01 21:53:13 +0100

    Added application::global_registry.

---
+++ include/bunsan/application/global_registry.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <bunsan/lazy_ptr.hpp>
+
+#include <boost/noncopyable.hpp>
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace bunsan{namespace application
+{
+    class global_state: private boost::noncopyable
+    {
+    public:
+        virtual ~global_state();
+
+    protected:
+        global_state();
+    };
+
+    template <typename T>
+    class global_state_any: public global_state
+    {
+    public:
+        explicit global_state_any(T state):
+            m_state(std::move(state)) {}
+
+        ~global_state_any() override {}
+
+    private:
+        T m_state;
+    };
+
+    using global_state_ptr = std::unique_ptr<global_state>;
+    using global_state_factory = std::function<global_state_ptr ()>;
+
+    template <typename T>
+    global_state_ptr make_global_any_state(T state)
+    {
+        std::unique_ptr<global_state> ptr(
+            new global_state_any<T>(std::move(state))
+        );
+        return ptr;
+    }
+
+    template <typename T>
+    global_state_ptr make_global_unique_state()
+    {
+        std::unique_ptr<T> ptr(new T{});
+        return make_global_any_state(std::move(ptr));
+    }
+
+    class global_registry: private boost::noncopyable
+    {
+    public:
+        using global_registry_ptr = std::shared_ptr<global_registry>;
+
+    public:
+        ~global_registry();
+
+    public:
+        /*!
+         * Locks state, lifetime is supported by external pointers.
+         * Once all pointers are destroyed registry is removed
+         * and next call will create it from scratch.
+         */
+        static global_registry_ptr lock();
+
+        /// \returns true
+        static bool register_state_factory(const global_state_factory &factory);
+
+        template <typename T>
+        static bool register_unique_state_factory()
+        {
+            return register_state_factory(
+                &make_global_unique_state<T>
+            );
+        }
+
+    private:
+        global_registry();
+
+    private:
+        std::vector<global_state_ptr> m_states;
+
+    private:
+        using state_factory_list = std::vector<global_state_factory>;
+
+        struct global_instance_storage
+        {
+            std::mutex lock;
+            state_factory_list factories;
+            std::weak_ptr<global_registry> instance;
+        };
+
+        static global_lazy_ptr<global_instance_storage> global_instance;
+    };
+
+    using global_registry_ptr = global_registry::global_registry_ptr;
+}}
---
+++ tests/global_registry.cpp
@@ -0,0 +1,42 @@
+#define BOOST_TEST_MODULE global_registry
+#include <boost/test/unit_test.hpp>
+
+#include <bunsan/application/global_registry.hpp>
+
+namespace ba = bunsan::application;
+
+namespace
+{
+    struct mock
+    {
+        static int ctor;
+        static int dtor;
+
+        mock() { ++ctor; }
+        ~mock() { ++dtor; }
+    };
+
+    int mock::ctor = 0;
+    int mock::dtor = 0;
+}
+
+BOOST_AUTO_TEST_SUITE(global_registry)
+
+BOOST_AUTO_TEST_CASE(register_)
+{
+    BOOST_CHECK(ba::global_registry::register_unique_state_factory<mock>());
+    BOOST_CHECK_EQUAL(mock::ctor, 0);
+    BOOST_CHECK_EQUAL(mock::dtor, 0);
+    {
+        const ba::global_registry_ptr reg = ba::global_registry::lock();
+        BOOST_CHECK_EQUAL(mock::ctor, 1);
+        BOOST_CHECK_EQUAL(mock::dtor, 0);
+        const ba::global_registry_ptr reg2 = ba::global_registry::lock();
+        BOOST_CHECK_EQUAL(mock::ctor, 1);
+        BOOST_CHECK_EQUAL(mock::dtor, 0);
+    }
+    BOOST_CHECK_EQUAL(mock::ctor, 1);
+    BOOST_CHECK_EQUAL(mock::dtor, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // global_registry
