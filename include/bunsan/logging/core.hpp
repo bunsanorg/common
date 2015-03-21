92ba7d34ae8db205d74ecb2b2cb099ab3ec4893d
Aleksey Filippov <sarum9in@gmail.com>
2015-03-21 16:55:38 +0100

    Added logging::set_minimum_severity().

--- include/bunsan/logging/core.hpp
+++ include/bunsan/logging/core.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <boost/log/core.hpp>
+#include <bunsan/logging/severity.hpp>
 
 namespace bunsan{namespace logging
 {
@@ -10,4 +10,6 @@ namespace bunsan{namespace logging
     inline bool disable() { return set_enabled(false); }
 
     void remove_default_sink();
+
+    void set_minimum_severity(const severity minimum_severity);
 }}
--- src/logging/core.cpp
+++ src/logging/core.cpp
@@ -1,6 +1,10 @@
 #include <bunsan/logging/core.hpp>
 
+#include <bunsan/logging/expressions/scope.hpp>
 #include <bunsan/logging/trivial.hpp>
+
+#include <boost/log/core.hpp>
+#include <boost/phoenix/bind.hpp>
 
 namespace bunsan{namespace logging
 {
@@ -12,5 +16,17 @@ namespace bunsan{namespace logging
     void remove_default_sink()
     {
         trivial::global::remove_default_sink();
+    }
+
+    void set_minimum_severity(const severity minimum_severity)
+    {
+        const auto filter =
+            [minimum_severity](boost::log::value_ref<severity, expressions::tag::severity> const &severity)
+            {
+                return !severity || severity >= minimum_severity;
+            };
+        boost::log::core::get()->set_filter(
+            boost::phoenix::bind(filter, expressions::severity.or_none())
+        );
     }
 }}
