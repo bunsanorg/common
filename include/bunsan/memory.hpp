305a106da21e8e34118476ba5026f1b8697d7d9a
Aleksey Filippov <sarum9in@gmail.com>
2015-05-19 23:54:06 +0200

    Added make_ptr().

---
+++ include/bunsan/memory.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <boost/intrusive_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <memory>
+#include <utility>
+
+namespace bunsan
+{
+    namespace detail
+    {
+        template <typename Pointer>
+        struct ptr_maker {};
+
+        template <typename T>
+        struct ptr_maker<std::unique_ptr<T>>
+        {
+            template <typename ... Args>
+            inline static std::unique_ptr<T> call(Args &&...args)
+            {
+                return std::make_unique<T>(std::forward<Args>(args)...);
+            }
+        };
+
+        template <typename T>
+        struct ptr_maker<std::shared_ptr<T>>
+        {
+            template <typename ... Args>
+            inline static std::shared_ptr<T> call(Args &&...args)
+            {
+                return std::make_shared<T>(std::forward<Args>(args)...);
+            }
+        };
+
+        template <typename T>
+        struct ptr_maker<boost::intrusive_ptr<T>>
+        {
+            template <typename ... Args>
+            inline static boost::intrusive_ptr<T> call(Args &&...args)
+            {
+                boost::intrusive_ptr<T> ptr(new T(std::forward<Args>(args)...));
+                return ptr;
+            }
+        };
+
+        template <typename T>
+        struct ptr_maker<boost::shared_ptr<T>>
+        {
+            template <typename ... Args>
+            inline static boost::shared_ptr<T> call(Args &&...args)
+            {
+                return boost::make_shared<T>(std::forward<Args>(args)...);
+            }
+        };
+    }
+
+    template <typename Pointer, typename ... Args>
+    inline Pointer make_ptr(Args &&...args)
+    {
+        return detail::ptr_maker<Pointer>::call(std::forward<Args>(args)...);
+    }
+}
